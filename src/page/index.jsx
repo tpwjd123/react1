82b6884602285e55803966f95acfe981e050d900
tpwjd123 <tpwjdskql@gmail.com>
2025-07-28 15:34:52 +0900

    Add main page component

---
+++ src/page/index.jsx
@@ -0,0 +1,230 @@
+import React from "react";
+import "./index.css";
+
+export default function Main() {
+  return (
+    <div className="main-container">
+      <div className="header">
+        <span className="ego-shop">Ego-Shop</span>
+        <div className="icons">
+          <div className="menu" />
+        </div>
+      </div>
+      <div className="banner">
+        <div className="image">
+          <div className="empty" />
+        </div>
+        <div className="block">
+          <span className="ribbon-tweed-dress">리본 트위드 2in1 원피스</span>
+          <div className="price">
+            <div className="box">
+              <span className="currency">₩</span>
+              <span className="comma">159,000</span>
+            </div>
+          </div>
+          <div className="rating" />
+          <div className="rectangle" />
+        </div>
+      </div>
+      <div className="ad" />
+      <div className="row">
+        <div className="flex-row-center">
+          <div className="card-sale">
+            <div className="image-1" />
+            <div className="text-box">
+              <span className="banded-pants-reorder">밴딩팬츠_리오더</span>
+              <div className="price-2">
+                <div className="box-3">
+                  <span className="currency-4">₩</span>
+                  <span className="comma-5">59,900</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="card-sale-6">
+            <div className="image-7" />
+            <div className="text-box-8">
+              <span className="span-band-pants-reorder">
+                데미지 셔링 데님 배기 팬츠
+              </span>
+              <div className="price-9">
+                <div className="box-a">
+                  <span className="span-won">₩</span>
+                  <span className="span-price">26,600</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex-row-cbc">
+          <div className="card-sale-b">
+            <div className="image-c" />
+            <div className="text-box-d">
+              <span className="span-long-flare-skirt">롱 플레어 스커트</span>
+              <div className="price-e">
+                <div className="box-f">
+                  <span className="span-won-10">₩</span>
+                  <span className="span-price-11">69,900</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="card-sale-12">
+            <div className="image-13" />
+            <div className="text-box-14">
+              <span className="span-linen-like-short-sleeve-jacket">
+                린넨라이크 반팔 자켓
+              </span>
+              <div className="price-15">
+                <div className="box-16">
+                  <span className="span-won-17">₩</span>
+                  <span className="span-price-18">79,900</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="ad-19" />
+      <span className="popular-product">인기상품</span>
+      <div className="icons-1a">
+        <div className="more" />
+      </div>
+      <div className="row-1b">
+        <div className="card">
+          <div className="image-1c" />
+          <span className="colorless-tweed-jacket">칼라리스 트위드 자켓</span>
+          <div className="price-1d">
+            <div className="box-1e">
+              <span className="won">₩</span>
+              <span className="comma-1f">99,900</span>
+            </div>
+          </div>
+        </div>
+        <div className="card-20">
+          <div className="image-21" />
+          <span className="linen-like-short-sleeve-jacket">
+            린넨라이크 반팔 자켓
+          </span>
+          <div className="price-22">
+            <div className="box-23">
+              <span className="won-24">₩</span>
+              <span className="comma-25">79,900</span>
+            </div>
+          </div>
+        </div>
+        <div className="card-26">
+          <div className="image-27" />
+          <span className="elastic-waist-pants-reorder">밴딩팬츠_리오더</span>
+          <div className="price-28">
+            <div className="box-29">
+              <span className="won-2a">₩</span>
+              <span className="comma-2b">59,900</span>
+            </div>
+          </div>
+        </div>
+        <div className="card-2c">
+          <div className="thumb">
+            <div className="image-2d" />
+            <div className="bg" />
+            <div className="icon">
+              <div className="icons-2e">
+                <div className="magnifier">
+                  <div className="vector" />
+                </div>
+              </div>
+              <span className="view-details">자세히보기</span>
+            </div>
+          </div>
+          <span className="collarless-tweed-jacket">칼라리스 트위드 자켓</span>
+          <div className="price-2f">
+            <div className="box-30">
+              <span className="won-31">₩</span>
+              <span className="price-99900">99,900</span>
+            </div>
+          </div>
+        </div>
+        <div className="card-32">
+          <div className="thumb-33">
+            <div className="img" />
+            <div className="bg-34" />
+            <div className="icon-35">
+              <div className="icons-36">
+                <div className="magnifier-37">
+                  <div className="vector-38" />
+                </div>
+              </div>
+              <span className="view-details-39">자세히보기</span>
+            </div>
+          </div>
+          <span className="linen-like-short-sleeve-jacket-3a">
+            린넨라이크 반팔 자켓
+          </span>
+          <div className="price-3b">
+            <div className="box-3c">
+              <span className="won-3d">₩</span>
+              <span className="price-79900">79,900</span>
+            </div>
+          </div>
+        </div>
+        <div className="card-3e">
+          <div className="thumb-3f">
+            <div className="img-40" />
+            <div className="bg-41" />
+            <div className="icon-42">
+              <div className="icons-43">
+                <div className="magnifier-44">
+                  <div className="vector-45" />
+                </div>
+              </div>
+              <span className="view-details-46">자세히보기</span>
+            </div>
+          </div>
+          <span className="collarless-tweed-jacket-reorder">
+            밴딩팬츠_리오더
+          </span>
+          <div className="price-47">
+            <div className="box-48">
+              <span className="currency-49">₩</span>
+              <span className="price-value">59,900</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="bottom-nav">
+        <div className="cart">
+          <div className="icons-4a">
+            <div className="cart-4b" />
+          </div>
+          <span className="cart-text">장바구니</span>
+          <div className="bg-4c" />
+        </div>
+        <div className="search">
+          <div className="icons-4d">
+            <div className="magnifier-4e" />
+          </div>
+          <span className="search-text">검색</span>
+          <div className="bg-4f" />
+        </div>
+        <div className="category">
+          <div className="icons-50">
+            <div className="category-51" />
+          </div>
+          <span className="category-text">카테고리</span>
+          <div className="bg-52" />
+        </div>
+        <div className="home-53">
+          <div className="icons-54">
+            <div className="home-55" />
+          </div>
+          <span className="home-text">홈</span>
+          <div className="bg-56" />
+        </div>
+      </div>
+      <span className="popular-products">할인상품</span>
+      <div className="icons-57">
+        <div className="more-58" />
+      </div>
+    </div>
+  );
+}
